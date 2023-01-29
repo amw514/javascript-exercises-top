7b77fe87cff225c04af38f35af5a315a954708a6
amw514 <alimunirwaris1@gmail.com>
2023-01-30 00:30:40 +0500

    06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 === 0 || (year % 100 !== 0 && year % 400 === 0)) {
+    console.log("is a leap year");
+  } else {
+    console.log("is not a leap year");
+  }
 };
+leapYears(2000);
 
 // Do not edit below this line
 module.exports = leapYears;
