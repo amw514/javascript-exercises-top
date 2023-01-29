77cc6d3ab461df20e1b99fcab790b6d5d037588d
amw514 <alimunirwaris1@gmail.com>
2023-01-29 14:44:45 +0500

    04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, num) {
+  return array.filter((x) => x !== num);
 };
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
