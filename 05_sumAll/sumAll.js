60e3f9c93575e8929e26504d5276be0610a9d709
amw514 <alimunirwaris1@gmail.com>
2023-01-29 22:59:36 +0500

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,11 @@
 const sumAll = function (min, max) {
   let sum = 0;
-  for (let i = min; i < max + 1; i++) {
+  for (i = min; i <= max; i++) {
     sum += i;
   }
   return sum;
 };
-sumAll(1, 4);
+sumAll(2, 4);
+
 // Do not edit below this line
 module.exports = sumAll;
