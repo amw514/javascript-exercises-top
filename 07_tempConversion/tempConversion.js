6655960266d34254fef603ecc44e0dad34fc7887
amw514 <alimunirwaris1@gmail.com>
2023-01-30 01:01:47 +0500

    07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (measure) {
+  let convert1 = measure - 32;
+  let convert2 = 5 / 9;
+  let combine = (convert1 * convert2).toFixed(1);
+  return combine;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (measure) {
+  let convert1 = measure * (9 / 5);
+  let convert2 = 32;
+  let combine = (convert1 + convert2).toFixed(1);
+  return combine;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
